067803c2e44ac2e9dc592cbdcaf2b3254aebb80c
Toha1234 <qqqolga.23.05.1992@gmail.com>
2021-11-14 15:52:14 +0400

    adding empty spring boot web application

---
+++ src/test/java/toha/shelepov/clientService/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package toha.shelepov.clientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
