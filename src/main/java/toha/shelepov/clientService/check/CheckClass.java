de71995dcf3f58ba53d1b9812416151604856ad8
Toha1234 <qqqolga.23.05.1992@gmail.com>
2021-12-05 17:49:47 +0400

    ANCS-2424: add CCC to CheckClass

--- src/main/java/toha/shelepov/clientService/check/CheckClass.java
+++ src/main/java/toha/shelepov/clientService/check/CheckClass.java
@@ -3,5 +3,6 @@ package toha.shelepov.clientService.check;
 public class CheckClass {
     public static void main(String[] args) {
         System.out.println("AAA");
+        System.out.println("CCC");
     }
 }
